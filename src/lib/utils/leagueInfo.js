0f89434d25c29b1f2242ec3cf31b431714b1b0d3
EnjoyMeSucka <enjoymesucka@yahoo.com>
2024-06-08 14:38:05 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -49,7 +49,7 @@ export const managers = [
      },
     {
      // "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-         "managerID": "416859641922732032"
+         "managerID": "416859641922732032" ,
       "name": Jones",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "West by God", // (optional)
@@ -57,9 +57,9 @@ export const managers = [
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 1944, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "mia", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
-        name: "Rival", // Can be anything (usually your rival's name)
+        name: "The Monsta From La Mancha", // Can be anything (usually your rival's name)
         link: 6, // manager array number within this array, or null to link back to all managers page
         image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
