f0a22169639961227ad2d66c366edc07e9894e51
EnjoyMeSucka <enjoymesucka@yahoo.com>
2024-06-08 12:46:13 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -22,7 +22,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
    {
-      // "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+     "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
      "managerID": "396524153546534912",
        "name": "Barbour",
   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
