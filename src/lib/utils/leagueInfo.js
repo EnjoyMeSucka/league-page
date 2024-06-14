93e58212cb41f779f30553bc516df6e397073ef6
EnjoyMeSucka <enjoymesucka@yahoo.com>
2024-06-14 18:04:45 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -30,7 +30,7 @@ export const managers = [
        "name": "TheDirtyHarry",
        "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "The Bone Yard", // (optional)
-       "bio": "Harold Francis "Dirty Harry" Callahan is a fictional San Francisco police detective and the main protagonist in the Dirty Harry film series, encompassing Dirty Harry (1971), Magnum Force (1973), The Enforcer (1976), Sudden Impact (1983), and The Dead Pool (1988). Callahan is portrayed by Clint Eastwood in each movie.",
+       "bio": "Harold Francis Callahan is a fictional San Francisco police detective and the main protagonist in the Dirty Harry film series, encompassing Dirty Harry (1971), Magnum Force (1973), The Enforcer (1976), Sudden Impact (1983), and The Dead Pool (1988). Callahan is portrayed by Clint Eastwood in each movie.",
        "photo": "/managers/DH.jpeg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 1995, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
